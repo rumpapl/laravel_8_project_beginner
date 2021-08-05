03ef454ee65b0939bbd8daa7570f54338c719333
RumpaPaul <rumpakucse14@gmail.com>
2021-08-05 17:34:14 +0600

    worked on showing input-field required error message right after each field

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,16 +1,18 @@
 <h1>Login page</h1>
 
-@if ($errors->any())
+<!-- @if ($errors->any())
     @foreach ($errors->all() as $err )
         <li>{{$err}}</li>
     @endforeach
-@endif
+@endif -->
 <form action="login" method='POST'>
 @csrf
 <input type="text" name='UserName' placeholder='user name'/>
+<span style="color: red">@error('UserName'){{$message}}@enderror</span>
 <br>
 <br>
 <input type="password" name='UserPassword' placeholder='user Password'/>
+<span style="color: red">@error('UserPassword'){{$message}}@enderror</span>
 <br>
 <br>
 <button type='submit'>Login</button>
