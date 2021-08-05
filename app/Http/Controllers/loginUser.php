ecdb6b2e6fa9fcf819d31d04b255ec4351d22d38
RumpaPaul <rumpakucse14@gmail.com>
2021-08-05 17:19:17 +0600

    worked on form input-field validation, show required field errors as list

--- app/Http/Controllers/loginUser.php
+++ app/Http/Controllers/loginUser.php
@@ -10,6 +10,11 @@ class loginUser extends Controller
 
     public function loginUserData(Request $req)
     {
+
+        $req->validate([
+            "UserName"=>'required',
+            "UserPassword"=>'required'
+        ]);
         return $req;
     }
 }
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,10 +1,16 @@
 <h1>Login page</h1>
+
+@if ($errors->any())
+    @foreach ($errors->all() as $err )
+        <li>{{$err}}</li>
+    @endforeach
+@endif
 <form action="login" method='POST'>
 @csrf
-<input type="text" name='User Name' placeholder='user name'/>
+<input type="text" name='UserName' placeholder='user name'/>
 <br>
 <br>
-<input type="password" name='User Password' placeholder='user Password'/>
+<input type="password" name='UserPassword' placeholder='user Password'/>
 <br>
 <br>
 <button type='submit'>Login</button>
